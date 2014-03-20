420cc8de0bc8115f615e48a0fa2ce9dff22c5c23
mhoonkang <myunghoon@wustl.edu>
2014-03-20 18:50:50 -0500

    summary.bma class
    hungry..

--- BmaPack/R/summary.bma.R
+++ BmaPack/R/summary.bma.R
@@ -13,3 +13,39 @@
 #' @aliases summary.bma-class initialize, summary.bma-method summary, summary.bma-method show, summary.bma-method print, summary.bma-method
 #' @rdname summary.bma
 #' @export
+setClass(Class="summary.bma",  
+         representation=representation(
+           expected.coefficient = "numeric",
+           posterior.prob = "numeric"
+         ),
+         prototype=prototype(
+           expected.coefficient = numeric(),
+           posterior.prob = numeric()
+         )
+)
+
+#' @export
+setMethod("initialize", "summary.bma", 
+          function(.Object, ...){
+            value=callNextMethod()
+            return(value)
+          }
+)
+
+
+#' @export
+setMethod("print","summary.bma", 
+          function(x, ...){
+            cat("The expected value of each coefficient: \n")
+            print(round(x@expected.coefficient,7))
+            cat("\n")
+            cat("The posterior probability that the coefficient is non-zero: \n")
+            print(round(x@posterior.prob,7))
+          }
+)
+
+#' @export
+setMethod("show","summary.bma", 
+          function(object){       
+            print(object)
+          })
