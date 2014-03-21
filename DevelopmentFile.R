72ba6e9c224ec2aeb458f29f0aa3fd474fdd7bfc
mhoonkang <myunghoon@wustl.edu>
2014-03-20 19:42:12 -0500

    comment on DevelopmentFile.R

--- DevelopmentFile.R
+++ DevelopmentFile.R
@@ -1,19 +1,24 @@
-rm(list=ls())
 library(devtools)
 library(roxygen2)
-find_rtools()
 current.code <- as.package("BmaPack")
 load_all(current.code)
 document(current.code)
 install(pkg=current.code, local=TRUE) 
 
+# check help documents
+?fitBMA
+?BmaPack
+
+# check function
 x <- matrix(rnorm(100,0,1),20,5)
 y <- rnorm(20,1,3)
 g <-3
 fitBMA(x,y,g)
 a <- fitBMA(x,y,g)
-str(a)
-plot(a)
 summary(a)
+plot(a)
+
+# check structure of bma class and summary.bma class
+str(a)
 b <- summary(a)
 str(b)
